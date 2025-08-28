e6b48f921b9d4a5cfa26ec5edf3ed2af418bcda5
ravillaanusha0-cloud <ravillaanusha0@gmail.com>
2025-08-28 17:15:49 +0530

    Update and rename pages/events/[id].js to pages/events.js

---
+++ pages/events.js
@@ -0,0 +1,29 @@
+// pages/events.js
+import { useEffect, useState } from "react"
+import { supabase } from "../lib/supabaseClient"
+
+export default function Events() {
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    fetchEvents()
+  }, [])
+
+  async function fetchEvents() {
+    const { data } = await supabase.from("events").select("*")
+    setEvents(data)
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Events</h2>
+      <ul>
+        {events?.map(ev => (
+          <li key={ev.eventid}>
+            <b>{ev.title}</b> - {ev.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
