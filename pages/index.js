e1c7aff2215dae48cd81a8bf9fe299b10ae28a78
ravillaanusha0-cloud <ravillaanusha0@gmail.com>
2025-08-28 17:12:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
+// pages/index.js
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Events App</h1>
+    <div style={{ padding: 20 }}>
+      <h1>Events App</h1>
       <ul>
-        <li><a href="/events">View Events</a></li>
-        <li><a href="/users">View Users</a></li>
-        <li><a href="/rsvps">View RSVPs</a></li>
+        <li><Link href="/users">Users</Link></li>
+        <li><Link href="/events">Events</Link></li>
+        <li><Link href="/rsvps">RSVPs</Link></li>
+        <li><Link href="/create-event">Create Event</Link></li>
       </ul>
     </div>
-  );
-}
+  )
+  }
